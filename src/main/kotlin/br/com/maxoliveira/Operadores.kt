0f93f7dc198d55cb97e25be16c0ef7ccc9d44a13
Max Oliveira <max-oliveira@outlook.com.br>
2023-03-30 12:12:45 -0300

    operadores

--- src/main/kotlin/br/com/maxoliveira/Operadores.kt
+++ src/main/kotlin/br/com/maxoliveira/Operadores.kt
@@ -7,10 +7,34 @@ fun main(){
 
     /* Operadores com Strings */
 
-
     println("Hello " +"Word")
-
     println("Outra forma de concatenação é ${"Usando essa notação"}")
+
+    /*Operadores Numéricos
+    *
+    *  + -> Somar
+    *  - -> Subtrair
+    *  * -> Multiplicar
+    *  / -> Dividir
+    *  % -> Resto da Divisão
+    *
+    * */
+
+    /* Operadores de Comparação
+    *  == -> Igual
+    *  !=  -> Diferente
+    *  >  -> Maior
+    *  <  -> Menor
+    *  >= -> Maior Igual
+    *  <= -> Menor Igual
+    * */
+
+
+    // CUIDADO COM COMPARAÇÃO DE ARRAY
+
+    println(arrayOf(1 ,2,3).contentEquals(arrayOf(1,2,3)))
+    println(arrayOf(1 ,2,3)==arrayOf(1,2,3))
+
 
 
 }
